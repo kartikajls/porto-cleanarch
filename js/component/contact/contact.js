87b8bc9c173bfc15e18f40872036dc7b1890596b
kartikajls <bagimu.negeriku12@gmail.com>
2026-09-22 17:06:25 +0700

    rev2

--- js/component/contact/contact.js
+++ js/component/contact/contact.js
@@ -2,25 +2,23 @@ export function Contact() {
     return `
         <section class="contact">
         
-            <div class="full-content">
-                <div class="contactus">
-                    <h2>Contact Us</h2>
-                    <table>
-                        <tr class="col-contactus">
-                            <td>📩</td>
-                            <td>Email</td>
-                            <td>:</td>
-                            <td>kartikajls.id@gmail.com</td>
-                        </tr>
-                        <tr class="col-contactus">
-                            <td>👤</td>
-                            <td>Linkedin</td>
-                            <td>:</td>
-                            <td><a href="https://www.linkedin.com/in/kartikajls/" class="mini">in/kartikajls</a></td>
-                        </tr>
-                    </table>
-                    <br>
-                </div>
+            <div class="contactus">
+                <h2>Contact</h2>
+                <table class="tab-contact">
+                    <tr class="col-contactus">
+                        <td>📩</td>
+                        <td>Email</td>
+                        <td>:</td>
+                        <td>kartikajls.id@gmail.com</td>
+                    </tr>
+                    <tr class="col-contactus">
+                        <td>👤</td>
+                        <td>Linkedin</td>
+                        <td>:</td>
+                        <td><a href="https://www.linkedin.com/in/kartikajls/" class="mini">in/kartikajls</a></td>
+                    </tr>
+                </table>
+                
             </div>
         
         </section>
